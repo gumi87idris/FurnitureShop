040027cb458e2808a21da00aec5ea96364dcbd1f
Baha <ggwpbaha@gmail.com>
2022-02-03 12:27:23 +0600

    registr

--- account/views.py
+++ account/views.py
@@ -1,12 +1,29 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .models import User
 from .forms import RegistrationForm
 
 
-class RegisterView(CreateView):
+class RegisterView(SuccessMessageMixin, CreateView):
     model = User
     template_name = 'account/registration.html'
     form_class = RegistrationForm
     success_url = reverse_lazy('home')
+    success_message = 'Успешно зарегистрирован!'
+
+
+class SignInView(LoginView):
+    template_name = 'account/login.html'
+    success_url = reverse_lazy('home')
+
+
+# class ProfileView(DetailView):
+#     model = User
+#     template_name = 'account/profile.html'
+
+def profile(request):
+    return render(request, 'account/profile.html')
