b6f78eceb6a364288cc5468a632de83022ad0875
gulmira <g.djumadylova@gmail.com>
2022-02-03 12:19:08 +0600

    commit t'
    exit

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+import cart.cart as Cart
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .models import Product
 
 
@@ -12,3 +14,49 @@ def shop(request):
         'pr': product
     }
     return render(request, 'shop.html', context)
+
+"""Cart views"""
+
+
+@login_required()
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required()
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required()
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required()       # если нам надо логинится на определенный логин, то пишем login_url=
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required()
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required()
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
